287427fa9ebd69af3a45939a263a54d3bfaad2c1
Kmishara <mishrashanvi7979@gmail.com>
2024-05-08 03:01:08 -0700

    added channel name

--- AdminPannel/Adminn/src/Components/Navbar/Navbar.jsx
+++ AdminPannel/Adminn/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from "../../assets/nav-logo.svg"
- import navprofile from '../../assets/nav-profile.svg'
+import navlogo from "../../assets/bag.jpg"
 const Navbar = () => {
   return (
+    <>
     <div className='Navbar'>
-        <img src={navlogo} alt="" className='nav-logo' />
-        {<img src={navprofile} className='nav-profile' alt="" /> }
+      <img src={navlogo} alt="" className='nav-logo' />
+        <h1>Admin Pannel</h1>
     </div>
+        <hr />
+    </>
   )
 }
 
